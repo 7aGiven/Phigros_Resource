19e9cf4bf3314fabb8522f0599812e71de64bf80
WalkerTian <903895182@qq.com>
2023-10-05 12:48:18 +0800

    我是傻逼

--- split.sh
+++ split.sh
@@ -6,7 +6,7 @@ end_offset=20 # 结尾不需要的部分，单位为秒
 for input in music/*.wav; do
     duration=$(ffmpeg -i "$input" 2>&1 | grep Duration | cut -d ' ' -f 4 | sed s/,//)
     length=$(echo "$duration" | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }' | cut -d '.' -f 1)
-    output_dir="splited_music/${input##*/}"
+    output_dir="splited_music/$(basename "${input%.*}")"
     mkdir -p "$output_dir"
     counter=1
     for ((start=start_offset; start<$length-end_offset; start+=$segment)); do
@@ -14,4 +14,7 @@ for input in music/*.wav; do
         echo "裁剪音频：${output_dir}/${counter}.wav"
         counter=$((counter+1))
     done
+    mv "$output_dir" "${output_dir%/*}/$(basename "${input%.*}")"
+    echo "移动文件夹：${output_dir} -> ${output_dir%/*}/$(basename "${input%.*}")"
 done
+

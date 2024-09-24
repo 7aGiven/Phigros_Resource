d76c7c1f9438c269903e0256d9f11ff1add2d984
7aGiven <fredrickareichert@gmail.com>
2024-09-24 22:34:47 +0800

    Long Time

--- gameInformation.py
+++ gameInformation.py
@@ -4,6 +4,7 @@ import sys
 from UnityPy import Environment
 import zipfile
 
+DEBUG = False
 
 class ByteReader:
     def __init__(self, data: bytes):
@@ -35,24 +36,33 @@ class ByteReader:
 
     def readSchema(self, schema: dict):
         result = []
-        for x in range(self.readInt()):
-            item = {}
-            for key, value in schema.items():
-                if value in (int, str, float):
-                    item[key] = self.d[value]()
-                elif type(value) == list:
-                    l = []
-                    for i in range(self.readInt()):
-                        l.append(self.d[value[0]]())
-                    item[key] = l
-                elif type(value) == tuple:
-                    for t in value:
-                        self.d[t]()
-                elif type(value) == dict:
-                    item[key] = self.readSchema(value)
-                else:
-                    raise Exception("无")
-            result.append(item)
+        mark = self.position
+        try:
+            for x in range(self.readInt()):
+                item = {}
+                for key, value in schema.items():
+                    if value in (int, str, float):
+                        item[key] = self.d[value]()
+                    elif type(value) == list:
+                        l = []
+                        for i in range(self.readInt()):
+                            l.append(self.d[value[0]]())
+                        item[key] = l
+                    elif type(value) == tuple:
+                        for t in value:
+                            self.d[t]()
+                    elif type(value) == dict:
+                        item[key] = self.readSchema(value)
+                    else:
+                        raise Exception("无")
+                    if DEBUG and type(key) != int:
+                        print(key, item[key])
+                result.append(item)
+        except Exception as e:
+            print(self.data[mark:self.position])
+            print("\n")
+            print(self.data[self.position:self.position+100])
+            raise e
         return result
 
 
@@ -73,15 +83,22 @@ def run(path):
             collection = data.raw_data.tobytes()
         elif data.m_Script.get_obj().read().name == "TipsProvider":
             tips = data.raw_data.tobytes()
+    if DEBUG:
+        with open("GameInformation", "wb") as f:
+            f.write(information)
+        with open("GetCollectionControl", "wb") as f:
+            f.write(collection)
 
     reader = ByteReader(information)
     reader.position = information.index(b"\x16\x00\x00\x00Glaciaxion.SunsetRay.0\x00\x00\n") - 4
     songBase_schema = {"songId": str, "songKey": str, "songName": str, "songTitle": str, "difficulty": [float],
-                       "illustrator": str, "charter": [str], "composer": str, "levels": [str], "previewTime": float,
-                       "unlockList": {"unlockType": int, "unlockInfo": [str]}, "levelMods": {"n": [str]}}
+                       "illustrator": str, "charter": [str], "composer": str, "levels": [str], "previewTime": float, "previewEndTime": float, 
+                       "unlockList": {"unlockType": int, "unlockInfo": [str]}, "levelMods": {"n": [str]}, "judgeLineImages": int, "isCnLimited": int,
+                       "hasDifferentMusic": int, "previewClipDifficulty": int, "hasDifferentCover": int, "differentCover": int}
     difficulty = []
     table = []
     for i in range(3):
+        print(reader.data[reader.position:reader.position+300])
         for item in reader.readSchema(songBase_schema):
             item["songId"] = item["songId"][:-2]
             if len(item["levels"]) == 5:
@@ -124,6 +141,8 @@ def run(path):
     with open("illustration.txt", "w", encoding="utf8") as f:
         for item in illustration:
             f.write("%s\n" % item)
+    print(single)
+    print(illustration)
 
     reader = ByteReader(collection)
     collection_schema = {1: (int, int, int, str, str, str), "key": str, "index": int, 2: (int,), "title": str,
@@ -138,13 +157,13 @@ def run(path):
         for key, value in D.items():
             f.write("%s\t%s\t%s\n" % (key, value[0], value[1]))
 
-    avatar_schema = {1: (int, int, int, str, int, str), "id": str, "file": str}
+    avatar_schema = {1: (int, int, int, str, str, str), "id": str, "file": str}
     table = reader.readSchema(avatar_schema)
     with open("avatar.txt", "w", encoding="utf8") as f:
         for item in table:
             f.write(item["id"])
             f.write("\n")
-    with open("tmp.tsv", "w") as f:
+    with open("tmp.tsv", "w", encoding="utf8") as f:
         for item in table:
             f.write("%s\t%s\n" % (item["id"], item["file"][7:]))
 

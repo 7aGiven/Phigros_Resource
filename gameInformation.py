5f2a3fcf59f08bf3f1fcd06cd0f7f33c8f93257a
7aGiven <fredrickareichert@gmail.com>
2024-10-03 22:15:19 +0800

    Class

--- gameInformation.py
+++ gameInformation.py
@@ -1,10 +1,10 @@
-import json
 import os
 import struct
-import subprocess
 import sys
 from UnityPy import Environment
 import zipfile
+
+import h
 
 DEBUG = False
 
@@ -12,7 +12,11 @@ class ByteReader:
     def __init__(self, data: bytes):
         self.data = data
         self.position = 0
-        self.d = {int: self.readInt, float: self.readFloat, str: self.readString}
+        self.d = {bool: self.readBool, int: self.readInt, float: self.readFloat, str: self.readString}
+    
+    def readBool(self):
+        self.position += 4
+        return self.data[self.position - 4] == 0
 
     def readInt(self):
         self.position += 4
@@ -29,43 +33,27 @@ class ByteReader:
         if length % 4 != 0:
             self.position += 4
         return result
-
-    def skipString(self):
-        length = self.readInt()
-        self.position += length // 4 * 4
-        if length % 4 != 0:
-            self.position += 4
-
-    def readSchema(self, schema: dict):
-        result = []
-        mark = self.position
-        try:
-            for x in range(self.readInt()):
-                item = {}
-                for key, value in schema.items():
-                    if value in (int, str, float):
-                        item[key] = self.d[value]()
-                    elif type(value) == list:
-                        l = []
-                        for i in range(self.readInt()):
-                            l.append(self.d[value[0]]())
-                        item[key] = l
-                    elif type(value) == tuple:
-                        for t in value:
-                            self.d[t]()
-                    elif type(value) == dict:
-                        item[key] = self.readSchema(value)
+    
+    def readClass(self, clazz):
+        obj = clazz()
+        for key, t in clazz.__annotations__.items():
+            if DEBUG:
+                print(key, t)
+            if type(t) == type:
+                setattr(obj, key, self.d[t]())
+            else:
+                l = []
+                t = t.__args__[0]
+                for _ in range(self.readInt()):
+                    if t in (bool, int, float, str):
+                        l.append(self.d[t]())
                     else:
-                        raise Exception("无")
-                    if DEBUG and type(key) != int:
-                        print(key, item[key])
-                result.append(item)
-        except Exception as e:
-            print(self.data[mark:self.position])
-            print("\n")
-            print(self.data[self.position:self.position+100])
-            raise e
-        return result
+                        l.append(self.readClass(t))
+                setattr(obj, key, l)
+            if DEBUG:
+                print(key, getattr(obj, key))
+        return obj
+
 
 
 def run(path):
@@ -93,27 +81,24 @@ def run(path):
 
     reader = ByteReader(information)
     reader.position = information.index(b"\x16\x00\x00\x00Glaciaxion.SunsetRay.0\x00\x00\n") - 4
-    songBase_schema = {"songId": str, "songKey": str, "songName": str, "songTitle": str, "difficulty": [float],
-                       "illustrator": str, "charter": [str], "composer": str, "levels": [str], "previewTime": float, "previewEndTime": float, 
-                       "unlockList": {"unlockType": int, "unlockInfo": [str]}, "levelMods": {"n": [str]}, "judgeLineImages": int, "isCnLimited": int,
-                       "hasDifferentMusic": int, "previewClipDifficulty": int, "hasDifferentCover": int, "differentCover": int}
+
     difficulty = []
     table = []
     for i in range(3):
-        print(reader.data[reader.position:reader.position+300])
-        for item in reader.readSchema(songBase_schema):
-            item["songId"] = item["songId"][:-2]
-            if len(item["levels"]) == 5:
-                item["difficulty"].pop()
-                item["charter"].pop()
-            if item["difficulty"][-1] == 0:
-                item["difficulty"].pop()
-                item["charter"].pop()
-            for i in range(len(item["difficulty"])):
-                item["difficulty"][i] = round(item["difficulty"][i], 1)
-            difficulty.append([item["songId"]] + item["difficulty"])
-            table.append((item["songId"], item["songName"], item["composer"], item["illustrator"], *item["charter"]))
-    reader.readSchema(songBase_schema)
+        for ii in range(reader.readInt()):
+            songItem = reader.readClass(h.SongsItem)
+            if len(songItem.difficulty) == 5:
+                songItem.difficulty.pop()
+                songItem.charter.pop()
+            if songItem.difficulty[-1] == 0.0:
+                songItem.difficulty.pop()
+                songItem.charter.pop()
+            for i in range(len(songItem.difficulty)):
+                songItem.difficulty[i] = str(round(songItem.difficulty[i], 1))
+            difficulty.append([songItem.songsId]+songItem.difficulty)
+            table.append((songItem.songsId, songItem.songsName, songItem.composer, songItem.illustrator, *songItem.charter))
+    for i in range(reader.readInt()):
+        reader.readClass(h.SongsItem)
 
     print(difficulty)
     print(table)
@@ -128,18 +113,19 @@ def run(path):
             f.write("\t".join(item))
             f.write("\n")
 
-    key_schema = {"key": str, "a": int, "type": int, "b": int}
     single = []
     illustration = []
-    for item in reader.readSchema(key_schema):
-        if item["type"] == 0:
-            single.append(item["key"])
-        elif item["type"] == 2 and item["key"] != "Introduction" and item["key"] not in single:
-            illustration.append(item["key"])
+    for i in range(reader.readInt()):
+        key = reader.readClass(h.Key)
+        if key.kindOfKey == 0:
+            single.append(key.keyName)
+        elif key.kindOfKey == 2 and key.keyName != "Introduction" and key.keyName not in single:
+            illustration.append(key.keyName)
 
     with open("single.txt", "w", encoding="utf8") as f:
         for item in single:
             f.write("%s\n" % item)
+
     with open("illustration.txt", "w", encoding="utf8") as f:
         for item in illustration:
             f.write("%s\n" % item)
@@ -147,29 +133,29 @@ def run(path):
     print(illustration)
 
     reader = ByteReader(collection)
-    collection_schema = {1: (int, int, int, str, str, str), "key": str, "index": int, 2: (int,), "title": str,
-                         3: (str, str, str, str)}
+
     D = {}
-    for item in reader.readSchema(collection_schema):
-        if item["key"] in D:
-            D[item["key"]][1] = item["index"]
+    for i in range(reader.readInt()):
+        item = reader.readClass(h.CollectionItemIndex)
+        if item.key in D:
+            D[item.key][1] = item.subIndex
         else:
-            D[item["key"]] = [item["title"], item["index"]]
+            D[item.key] = [item.chinese, item.subIndex]
+
     with open("collection.tsv", "w", encoding="utf8") as f:
         for key, value in D.items():
             f.write("%s\t%s\t%s\n" % (key, value[0], value[1]))
 
-    avatar_schema = {1: (int, int, int, str, str, str), "id": str, "file": str}
-    table = reader.readSchema(avatar_schema)
-    with open("avatar.txt", "w", encoding="utf8") as f:
-        for item in table:
-            f.write(item["id"])
-            f.write("\n")
-    with open("tmp.tsv", "w", encoding="utf8") as f:
-        for item in table:
-            f.write("%s\t%s\n" % (item["id"], item["file"][7:]))
+    with open("avatar.txt", "w", encoding="utf8") as avatar:
+        with open("tmp.tsv", "w", encoding="utf8") as tmp:
+            for i in range(reader.readInt()):
+                item = reader.readClass(h.AvatarInfo)
+                avatar.write(item.name)
+                avatar.write("\n")
+                tmp.write("%s\t%s\n" % (item.name, item.addressableKey[7:]))
 
     reader = ByteReader(tips[8:])
+
     with open("tips.txt", "w", encoding="utf8") as f:
         for i in range(reader.readInt()):
             f.write(reader.readString())
@@ -178,6 +164,7 @@ def run(path):
 
 if __name__ == "__main__":
     if len(sys.argv) == 1 and os.path.isdir("/data/"):
+        import subprocess
         r = subprocess.run("pm path com.PigeonGames.Phigros",stdin=subprocess.DEVNULL,stdout=subprocess.PIPE,stderr=subprocess.DEVNULL,shell=True)
         path = r.stdout[8:-1].decode()
     else:
---
+++ h.py
@@ -0,0 +1,60 @@
+class ChartUnlock:
+	unlockType: int
+	unlockInfo: list[str]
+
+class LevelMods:
+	levelMods: list[str]
+
+class SongsItem:
+	songsId: str
+	songsKey: str
+	songsName: str
+	songsTitle: str
+	difficulty: list[float]
+	illustrator: str
+	charter: list[str]
+	composer: str
+	levels: list[str]
+	previewTime: float
+	previewEndTime: float
+	unlockInfo: list[ChartUnlock]
+	# judgeLineImages
+	levelMods: list[LevelMods]
+	isCnLimited: bool
+	hasDifferentMusic: bool
+	differentMusic: int
+	previewClipDifficulty: int
+	hasDifferentCover: bool
+	differentCover: int
+
+class Key:
+	keyName: str
+	unlockedTimes: int
+	kindOfKey: int
+	unlockTimes: int
+
+class CollectionItemIndex:
+	getSong: int
+	getType: int
+	getPower: int
+	getInfo1: str
+	getInfo2: str
+	getInfo3: str
+	key: str
+	subIndex: int
+	code: str
+	chinese: str
+	chineseTraditional: str
+	english: str
+	japanese: str
+	korean: str
+
+class AvatarInfo:
+	getSong: int
+	getType: int
+	getPower: int
+	getInfo1: str
+	getInfo2: str
+	getInfo3: str
+	name: str
+	addressableKey: str

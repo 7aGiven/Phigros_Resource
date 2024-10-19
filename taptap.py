4f42f7995039e95bef1ac28d7a838285c91ff347
7aGiven <fredrickareichert@gmail.com>
2024-10-19 12:39:36 +0800

    taptap

--- taptap.py
+++ taptap.py
@@ -10,13 +10,25 @@ import uuid
 sample = string.ascii_lowercase + string.digits
 
 def taptap(apkid):
-    nonce = "".join(random.sample(sample, 5))
     uid = uuid.uuid4()
-    t = int(time.time())
     X_UA = "V=1&PN=TapTap&VN_CODE=206012000&LOC=CN&LANG=zh_CN&CH=default&UID=%s" % uid
+    
+    req = urllib.request.Request(
+        "https://api.taptapdada.com/app/v2/detail-by-id/%d?X-UA=%s" % (appid, urllib.parse.quote(X_UA)),
+        headers={"User-Agent": "okhttp/3.12.1"}
+    )
+    with urllib.request.urlopen(req) as response:
+        r = json.load(response)
+    print(r["data"]["download"])
+    apkid = r["data"]["download"]["apk_id"]
+    
+
+    nonce = "".join(random.sample(sample, 5))
+    t = int(time.time())
     byte = "X-UA=%s&end_point=d1&id=%d&node=%s&nonce=%s&time=%sPeCkE6Fu0B10Vm9BKfPfANwCUAn5POcs" % (X_UA, apkid, uid, nonce, t)
     md5 = hashlib.md5(byte.encode()).hexdigest()
     body = "sign=%s&node=%s&time=%s&id=%d&nonce=%s&end_point=d1" % (md5, uid, t, apkid, nonce)
+
     req = urllib.request.Request(
         "https://api.taptapdada.com/apk/v1/detail?X-UA=" + urllib.parse.quote(X_UA),
         body.encode(),
@@ -26,7 +38,8 @@ def taptap(apkid):
         return json.load(response)
 
 
-# Phigros apk id = 926396
+
+# Phigros app id = 165287
 if __name__ == "__main__":
-    r = taptap(926396)
+    r = taptap(165287)
     print(r)

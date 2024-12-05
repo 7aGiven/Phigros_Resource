9399e4109ecc50ce5ccfb694ca18a782c90c9403
7aGiven <fredrickareichert@gmail.com>
2024-12-05 13:29:22 +0800

    typetree

--- gameInformation.py
+++ gameInformation.py
@@ -1,63 +1,14 @@
+import json
 import os
-import struct
 import sys
 from UnityPy import Environment
 import zipfile
 
-import h
-
-class ByteReader:
-    def __init__(self, data: bytes):
-        self.data = data
-        self.position = 0
-        self.d = {bool: self.readBool, int: self.readInt, float: self.readFloat, str: self.readString}
-    
-    def readBool(self):
-        self.position += 4
-        return self.data[self.position - 4] == 0
-
-    def readInt(self):
-        self.position += 4
-        return self.data[self.position - 4] ^ self.data[self.position - 3] << 8
-
-    def readFloat(self):
-        self.position += 4
-        return struct.unpack("f", self.data[self.position - 4:self.position])[0]
-
-    def readString(self):
-        length = self.readInt()
-        result = self.data[self.position:self.position + length].decode()
-        self.position += length // 4 * 4
-        if length % 4 != 0:
-            self.position += 4
-        return result
-    
-    def readClass(self, clazz):
-        obj = clazz()
-        for key, t in clazz.__annotations__.items():
-            if not __debug__:
-                print(key, t)
-            if type(t) == type:
-                if t in (bool, int, float, str):
-                    setattr(obj, key, self.d[t]())
-                else:
-                    setattr(obj, key, self.readClass(t))
-            else:
-                l = []
-                t = t.__args__[0]
-                for _ in range(self.readInt()):
-                    if t in (bool, int, float, str):
-                        l.append(self.d[t]())
-                    else:
-                        l.append(self.readClass(t))
-                setattr(obj, key, l)
-            if not __debug__:
-                print(key, getattr(obj, key))
-        return obj
-
-
+DEBUG = False
 
 def run(path):
+    with open("typetree.json") as f:
+        typetree = json.load(f)
     env = Environment()
     with zipfile.ZipFile(path) as apk:
         with apk.open("assets/bin/Data/globalgamemanagers.assets") as f:
@@ -69,38 +20,27 @@ def run(path):
             continue
         data = obj.read()
         if data.m_Script.get_obj().read().name == "GameInformation":
-            information = data.raw_data.tobytes()
+            GameInformation = obj.read_typetree(typetree["GameInformation"])
         elif data.m_Script.get_obj().read().name == "GetCollectionControl":
-            collection = data.raw_data.tobytes()
+            Collections = obj.read_typetree(typetree["GetCollectionControl"], True)
         elif data.m_Script.get_obj().read().name == "TipsProvider":
-            tips = data.raw_data.tobytes()
-    if not __debug__:
-        with open("GameInformation", "wb") as f:
-            f.write(information)
-        with open("GetCollectionControl", "wb") as f:
-            f.write(collection)
-
-    reader = ByteReader(information)
-    reader.position = information.index(b"\x16\x00\x00\x00Glaciaxion.SunsetRay.0\x00\x00\n") - 4
+            Tips = obj.read_typetree(typetree["TipsProvider"], True)
 
     difficulty = []
     table = []
-    for i in range(3):
-        for ii in range(reader.readInt()):
-            songItem = reader.readClass(h.SongsItem)
-            songItem.songsId = songItem.songsId[:-2]
-            if len(songItem.difficulty) == 5:
-                songItem.difficulty.pop()
-                songItem.charter.pop()
-            if songItem.difficulty[-1] == 0.0:
-                songItem.difficulty.pop()
-                songItem.charter.pop()
-            for i in range(len(songItem.difficulty)):
-                songItem.difficulty[i] = str(round(songItem.difficulty[i], 1))
-            difficulty.append([songItem.songsId]+songItem.difficulty)
-            table.append((songItem.songsId, songItem.songsName, songItem.composer, songItem.illustrator, *songItem.charter))
-    for i in range(reader.readInt()):
-        reader.readClass(h.SongsItem)
+    for key, songs in GameInformation["song"].items():
+        if key == "otherSongs":
+            continue
+        for song in songs:
+            if len(song["difficulty"]) == 5:
+                song["difficulty"].pop()
+            if song["difficulty"][-1] == 0.0:
+                song["difficulty"].pop()
+                song["charter"].pop()
+            for i in range(len(song["difficulty"])):
+                song["difficulty"][i] = str(round(song["difficulty"][i], 1))
+            difficulty.append([song["songsId"]]+song["difficulty"])
+            table.append((song["songsId"], song["songsName"], song["composer"], song["illustrator"], *song["charter"]))
 
     print(difficulty)
     print(table)
@@ -117,12 +57,11 @@ def run(path):
 
     single = []
     illustration = []
-    for i in range(reader.readInt()):
-        key = reader.readClass(h.Key)
-        if key.kindOfKey == 0:
-            single.append(key.keyName)
-        elif key.kindOfKey == 2 and key.keyName != "Introduction" and key.keyName not in single:
-            illustration.append(key.keyName)
+    for key in GameInformation["keyStore"]:
+        if key["kindOfKey"] == 0:
+            single.append(key["keyName"])
+        elif key["kindOfKey"] == 2 and key["keyName"] != "Introduction" and key["keyName"] not in single:
+            illustration.append(key["keyName"])
 
     with open("info/single.txt", "w", encoding="utf8") as f:
         for item in single:
@@ -134,11 +73,8 @@ def run(path):
     print(single)
     print(illustration)
 
-    reader = ByteReader(collection)
-
     D = {}
-    for i in range(reader.readInt()):
-        item = reader.readClass(h.CollectionItemIndex)
+    for item in Collections.collectionItems:
         if item.key in D:
             D[item.key][1] = item.subIndex
         else:
@@ -150,17 +86,14 @@ def run(path):
 
     with open("info/avatar.txt", "w", encoding="utf8") as avatar:
         with open("info/tmp.tsv", "w", encoding="utf8") as tmp:
-            for i in range(reader.readInt()):
-                item = reader.readClass(h.AvatarInfo)
+            for item in Collections.avatars:
                 avatar.write(item.name)
                 avatar.write("\n")
                 tmp.write("%s\t%s\n" % (item.name, item.addressableKey[7:]))
 
-    reader = ByteReader(tips[8:])
-
     with open("info/tips.txt", "w", encoding="utf8") as f:
-        for i in range(reader.readInt()):
-            f.write(reader.readString())
+        for tip in Tips.tips[0].tips:
+            f.write(tip)
             f.write("\n")
 
 

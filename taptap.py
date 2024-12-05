a3991c9fe5acb89d18f4080fed1826c7f230f71b
7aGiven <fredrickareichert@gmail.com>
2024-12-05 10:21:32 +0800

    taptap

--- taptap.py
+++ taptap.py
@@ -1,10 +1,10 @@
 import hashlib
+from http.client import HTTPSConnection
 import json
 import random
 import string
 import time
 import urllib.parse
-import urllib.request
 import uuid
 
 sample = string.ascii_lowercase + string.digits
@@ -13,15 +13,17 @@ def taptap(appid):
     uid = uuid.uuid4()
     X_UA = "V=1&PN=TapTap&VN_CODE=206012000&LOC=CN&LANG=zh_CN&CH=default&UID=%s" % uid
     
-    req = urllib.request.Request(
-        "https://api.taptapdada.com/app/v2/detail-by-id/%d?X-UA=%s" % (appid, urllib.parse.quote(X_UA)),
+    conn = HTTPSConnection("api.taptapdada.com")
+    conn.request(
+        "GET",
+        "/app/v2/detail-by-id/%d?X-UA=%s" % (appid, urllib.parse.quote(X_UA)),
         headers={"User-Agent": "okhttp/3.12.1"}
     )
-    with urllib.request.urlopen(req) as response:
-        r = json.load(response)
+    r = json.load(conn.getresponse())
     print(r["data"]["download"])
     apkid = r["data"]["download"]["apk_id"]
-    
+
+    print("")
 
     nonce = "".join(random.sample(sample, 5))
     t = int(time.time())
@@ -29,17 +31,16 @@ def taptap(appid):
     md5 = hashlib.md5(byte.encode()).hexdigest()
     body = "sign=%s&node=%s&time=%s&id=%d&nonce=%s&end_point=d1" % (md5, uid, t, apkid, nonce)
 
-    req = urllib.request.Request(
-        "https://api.taptapdada.com/apk/v1/detail?X-UA=" + urllib.parse.quote(X_UA),
-        body.encode(),
-        {"User-Agent": "okhttp/3.12.1"}
+    conn.request(
+        "POST",
+        "/apk/v1/detail?X-UA=" + urllib.parse.quote(X_UA),
+        body=body.encode(),
+        headers={"Content-Type": "application/x-www-form-urlencoded", "User-Agent": "okhttp/3.12.1"}
     )
-    with urllib.request.urlopen(req) as response:
-        return json.load(response)
-
-
+    r = json.load(conn.getresponse())
+    print(r)
 
 # Phigros app id = 165287
 if __name__ == "__main__":
-    r = taptap(165287)
-    print(r)
+    taptap(165287)
+

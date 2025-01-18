3041344d8b003c86a81c3473dc392735296272a7
Shua-github <CHA_shua@outlook.com>
2025-01-19 02:43:34 +0800

    修复phira.py,使用更好的字符串拼接

--- phira.py
+++ phira.py
@@ -4,31 +4,58 @@ from zipfile import ZipFile
 
 levels = ["EZ", "HD", "IN", "AT"]
 
+# 删除旧目录并创建新目录
 shutil.rmtree("phira", True)
 os.mkdir("phira")
 for level in levels:
-    os.mkdir("phira/%s" %level)
+    os.mkdir(f"phira/{level}")
 
+# 读取并解析 info.tsv 文件
 infos = {}
-with open("info.tsv", encoding="utf8") as f:
+with open("info/info.tsv", encoding="utf8") as f:
     while True:
         line = f.readline()
         if not line:
             break
         line = line[:-1].split("\t")
-        infos[line[0]] = {"Name": line[1], "Composer": line[2], "Illustrator": line[3], "Chater": line[4:]}
-with open("difficulty.tsv", encoding="utf8") as f:
+        infos[line[0]] = {
+            "Name": line[1],
+            "Composer": line[2],
+            "Illustrator": line[3],
+            "Chater": line[4:]
+        }
+
+# 读取并解析 difficulty.tsv 文件
+with open("info/difficulty.tsv", encoding="utf8") as f:
     while True:
         line = f.readline()
         if not line:
             break
         line = line[:-1].split("\t")
         infos[line[0]]["difficulty"] = line[1:]
+
+# 创建 .pez 文件
 for id, info in infos.items():
     print(info["Name"], info["Composer"])
-    for level in range(len(info["difficulty"])):
-        with ZipFile("phira/%s/%s-%s.pez" % (levels[level], id, levels[level]), "x") as pez:
-            pez.writestr("info.txt", "#\nName: %s\nSong: %s.ogg\nPicture: %s.png\nChart: %s.json\nLevel: %s Lv.%s\nComposer: %s\nIllustrator: %s\nCharter: %s" % (info["Name"], id, id, id, levels[level], info["difficulty"][level], info["Composer"], info["Illustrator"], info["Chater"][level]))
-            pez.write("Chart_%s/%s.0.json" % (levels[level], id), "%s.json" % id)
-            pez.write("IllustrationLowRes/%s.png" % id, "%s.png" % id)
-            pez.write("music/%s.ogg" % id, "%s.ogg" % id)
+    for level_index in range(len(info["difficulty"])):
+        level = levels[level_index]
+        pez_path = f"phira/{level}/{id}-{level}.pez"
+        with ZipFile(pez_path, "x") as pez:
+            # 写入 info.txt 内容
+            info_txt_content = (
+                f"#\n"
+                f"Name: {info['Name']}\n"
+                f"Song: {id}.ogg\n"
+                f"Picture: {id}.png\n"
+                f"Chart: {id}.json\n"
+                f"Level: {level} Lv.{info['difficulty'][level_index]}\n"
+                f"Composer: {info['Composer']}\n"
+                f"Illustrator: {info['Illustrator']}\n"
+                f"Charter: {info['Chater'][level_index]}"
+            )
+            pez.writestr("info.txt", info_txt_content)
+
+            # 添加文件到 .pez 压缩包
+            pez.write(f"chart/{id}.0/{level}.json", f"{id}.json")
+            pez.write(f"IllustrationLowRes/{id}.png", f"{id}.png")
+            pez.write(f"music/{id}.ogg", f"{id}.ogg")
